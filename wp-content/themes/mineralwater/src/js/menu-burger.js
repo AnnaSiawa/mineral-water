ffe55db022c8f077eff2046f00c41a96c0355269
Anna <a.kucheryavenkova@m-games-ltd.com>
2022-12-02 16:39:30 +0300

    add header

--- wp-content/themes/mineralwater/src/js/menu-burger.js
+++ wp-content/themes/mineralwater/src/js/menu-burger.js
@@ -1,10 +1,6 @@
 window.onload = function () {
     const iconMenu = document.querySelector('.menu__icon');
-    const bodyMenu = document.querySelector('.mobile-menu');
-    const menuCell = document.querySelector('.mobile-menu-cell');
-    const contactsMenu = document.querySelector('.contacts-menu');
-    const contactsItem = document.querySelectorAll('.contacts-item');
-    const photoItem = document.querySelector('.photo-item');
+    const bodyMenu = document.querySelector('.mobile-menu__nav');
 
     //mobile menu
     const toggleMenu = () => {
@@ -12,8 +8,6 @@ window.onload = function () {
         document.getElementsByTagName('html')[0].classList.toggle('lock');
         bodyMenu.classList.toggle('active');
         iconMenu.classList.toggle('active');
-        menuCell.classList.toggle('active');
-        contactsMenu.classList.remove('active');
     }
 
     if (iconMenu) {
@@ -21,28 +15,6 @@ window.onload = function () {
             e.stopPropagation();
             toggleMenu();
         });
-
-        photoItem.addEventListener('click', e => {
-            e.stopPropagation();
-            toggleMenu();
-        });
-
-        //прокрутка до блока Наши работы
-        const itemPhotoScroll = document.querySelector('.photo-item[data-goto]');
-        itemPhotoScroll.addEventListener('click', onItemScrollPhoto);
-
-        function onItemScrollPhoto(e) {
-            const itemPhotoScroll = e.currentTarget;
-            const gotoBlock = document.querySelector(itemPhotoScroll.dataset.goto);
-            if (gotoBlock) {
-                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
-
-                window.scrollTo({
-                    top: gotoBlockValue - 50,
-                    // behavior: 'smooth'
-                });
-            }
-        }
 
         document.addEventListener('click', e => {
             let target = e.target;
@@ -53,30 +25,8 @@ window.onload = function () {
             if (!itsMenu && !itsIconMenu && menuIsActive) {
                 bodyMenu.classList.toggle('active');
                 iconMenu.classList.toggle('active');
-                menuCell.classList.toggle('active');
-                contactsMenu.classList.remove('active');
             }
         });
     }
-
-    //contacts mobile and desktop menu
-    const toggleContacts = () => {
-        contactsMenu.classList.toggle('active');
-    }
-
-    contactsItem.forEach(item => {
-        item.addEventListener('click', e => {
-            e.stopPropagation();
-            toggleContacts();
-        });
-        document.addEventListener('click', e => {
-            let target = e.target;
-            let itsContactsMenu = target === contactsMenu || contactsMenu.contains(target);
-            let menuIsActive = contactsMenu.classList.contains('active');
-            if (!itsContactsMenu && menuIsActive) {
-                contactsMenu.classList.toggle('active');
-            }
-        });
-    });
 
 }
